50cd859639402f41abb3f777fc626d6294a86e6a
aplumley12 <134178344+aplumley12@users.noreply.github.com>
2023-06-12 22:44:23 -0400

    Update and rename Loops Code.cpp to Functions Code.cpp

---
+++ Functions/Functions Code.cpp
@@ -0,0 +1,23 @@
+/////// IMPORTS ///////
+#include <iostream>
+#include <istream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+/////// FUNCTION DEFINITIONS ///////
+
+// Example (1): using a function to greet the user
+void greeting(string name) {
+  cout << "Hello, " << name << endl;
+}
+
+/////// MAIN FUNCTION ///////
+int main() {
+  /*
+  This is the main function; it calls all the user-defined functions in this file
+  */
+  greeting("Mary Sue");
+}
+  
